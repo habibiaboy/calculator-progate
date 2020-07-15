8af9d2ab24f813362c44351a140fc4d34527ed40
habibiaboy <muhammad.rifqi@bpjsketenagakerjaan.go.id>
2020-07-15 21:38:38 +0700

    final exam in progate commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# calculator-progate
+Create simple Calculator
+
+## Screenshot
+
+![ScreenShot](/img/final-exam.png)
+
+## Authors
+
+* **habibi** - *Initial work* - [Gentengman](https://github.com/habibiaboy)
+
---
+++ main.js
@@ -0,0 +1,126 @@
+const calculatorScreen = document.querySelector('.calculator-screen');
+
+const updateScreen = (number) => {
+	calculatorScreen.value = number
+}
+
+
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach((number) => {
+	number.addEventListener("click",(event) => {
+		inputNumber(event.target.value)
+		updateScreen(currentNumber)
+		
+	})
+})
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+	if (currentNumber === '0'){
+		currentNumber = number
+	} else {
+		currentNumber += number
+	}
+}
+
+
+const operators = document.querySelectorAll('.operator')
+
+operators.forEach ((operator) => {
+	operator.addEventListener('click', (event) => {
+		//console.log(event.target.value)
+		inputOperator(event.target.value)
+	})
+})
+
+const inputOperator = (operator) => {
+	if (calculationOperator === ''){
+		prevNumber = currentNumber
+	}
+	calculationOperator = operator
+	currentNumber = '0'
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+
+equalSign.addEventListener('click', () => {
+	calculate()
+	updateScreen(currentNumber)
+	// console.log('Equal Pencet')
+ })
+
+
+const calculate = () => {
+	let result = ''
+	switch(calculationOperator){
+		case "+":
+			result = parseFloat(prevNumber) + parseFloat(currentNumber)
+			break
+		case "-":
+			result = parseFloat(prevNumber) - parseFloat(currentNumber)
+			break
+		case "*":
+			result = parseFloat(prevNumber) * parseFloat(currentNumber)
+			break
+		case "/":
+			result = parseFloat(prevNumber) / parseFloat(currentNumber)
+			break
+		default:
+			return
+	}
+	currentNumber = result
+	calculationOperator = ''
+}
+
+const clearButton = document.querySelector('.all-clear');
+
+clearButton.addEventListener('click',()=> {
+	clearAll()
+	updateScreen(currentNumber)
+	//console.log('Button On')
+})
+
+const clearAll = () => {
+	prevNumber = ''
+	calculationOperator = ''
+	currentNumber = '0'
+}
+
+const decimal = document.querySelector('.decimal');
+
+decimal.addEventListener('click',(event) => {
+	inputDecimal(event.target.value)
+	updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+	currentNumber += dot
+}
+
+const percent = document.querySelector('.percentage');
+
+percent.addEventListener('click',(event) => {
+	percentageNumber()
+	updateScreen(currentNumber)
+})
+
+const percentageNumber = () => {
+	if(currentNumber === 0 ){
+		return
+	}
+	currentNumber = currentNumber / 100
+}
+
+
+
+
+
+
+
+
+
